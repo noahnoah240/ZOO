cd48b3b8f666ebd41a1a2d9c59a5cf237f012679
[200~noah1234567890987654321234567890534233~ <noah.megroedt@student.sjabi.be>
2026-05-23 18:43:54 +0200

    Aparte pagina's voor elke katachtige, veranderingen in css

--- app.py
+++ app.py
@@ -5,32 +5,33 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 #maak arrays voor de verschillende categorieën dieren
-CATEGORIEEN = ["Katachtigen", "Mensapen", "Beren", "Kleine roofdieren", "Zeedieren", "Hoefdieren", "Vogels", "Reptielen", "Aquariumdieren"]
+CATEGORIEEN = ["Katachtigen", "Mensapen", "Beren", "Roofdieren", "Zeedieren", "Hoefdieren", "Vogels", "Reptielen", "Aquariumdieren"]
 KATACHTIGEN = ["Leeuw", "Amoertijger", "Jaguar", "Amoerluipaard"]
 MENSAPEN = ["Gorilla", "Chimpansee", "Uilenkopmeerkat"]
 BEREN = ["Brilbeer"]
-KLEINE_ROOFDIEREN = ["Rode panda", "Stokstaartje", "Neusbeer", "Vosmangoest", "Wasbeer"]
+ROOFDIEREN = ["Rode panda", "Stokstaartje", "Neusbeer", "Vosmangoest", "Wasbeer"]
 ZEEDIEREN = ["Californische zeeleeuw", "Gewone zeehond"]
 HOEFDIEREN = ["Giraffe", "Okapi", "Kaapse buffel", "Anoa", "Nijlpaard", "Olifant"]
 VOGELS = ["Pinguïn", "Afrikaanse vogelsoorten"]
 REPTIELEN = ["Krokodil", "Alligator", "Slang", "Hagedis"]
 AQUARIUMDIEREN = ["Tripische rifvissen", "Koralen", "Zoet- en zoutwatervissen"]
-#route voor de indexpagina met categorieën
+#route van startpagina
 @app.route("/")
 def index():
-    return render_template("index.html", categorieen=CATEGORIEEN, katachtigen=KATACHTIGEN, mensapen=MENSAPEN, beren=BEREN, kleine_roofdieren=KLEINE_ROOFDIEREN, zeedieren=ZEEDIEREN, hoefdieren=HOEFDIEREN , vogels=VOGELS, reptielen=REPTIELEN, aquariumdieren=AQUARIUMDIEREN)
-#route voor verschillende categorieën 
-#route voor katachtigen
-@app.route("/Katachtigen")
-def katachtigen():
-    return render_template("Katachtigen.html", katachtigen=KATACHTIGEN)
-#route voor mensapen
-@app.route("/Mensapen")
-def mensapen():
-    return render_template("mensapen.html", mensapen=MENSAPEN)
-@app.route("/Categorie")
-def categorie():
-    return render_template("Categorie.html")
+    return render_template("index.html", categorieen=CATEGORIEEN, katachtigen=KATACHTIGEN, mensapen=MENSAPEN, beren=BEREN, roofdieren=ROOFDIEREN, zeedieren=ZEEDIEREN, hoefdieren=HOEFDIEREN , vogels=VOGELS, reptielen=REPTIELEN, aquariumdieren=AQUARIUMDIEREN)
+#route van pagina met dieren
+@app.route("/Dieren")
+def dieren():
+    return render_template("Dieren.html")
+#route van pagina met kaart
 @app.route("/Kaart")
 def kaart():
     return render_template("Kaart.html")
+#route van elke pagina van elk individueel dier
+for categorie in CATEGORIEEN: #loop door elke categorie heen
+    for dier in eval(categorie.upper()): #loop door elk dier in de categorie heen
+        def dierpagina(d=dier): 
+            return render_template("dierpagina.html", dier=d)
+        
+        dierpagina.__name__ = f"dierpagina_{dier}"
+        app.add_url_rule(f"/{dier}", endpoint=dier, view_func=dierpagina)
--- static/script.js
+++ static/script.js
@@ -1,9 +1,13 @@
+//event-listener en input/output definieren
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("grid-toggle");
   const grid = document.getElementById("grid-container");
   if (!button || !grid) return;
-
+//definieren wat er gebeurt nadat je de knop indrukt
   button.addEventListener("click", () => {
     grid.classList.toggle("visible");
   });
 });
+
+const vollePath = window.location.pathname; // neemt de route
+const laatste = vollePath.split("/").filter(Boolean).pop(); // neemt het laatste deel van de route
